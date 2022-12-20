5fc52272951ee996deaf5825846461f0da0ea208
Jitesh Nahire <Jiteshnahire@gmail.com>
2022-12-20 13:10:25 +0530

    Add files via upload

---
+++ StaticProgram.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillMineProject7.StaticVariable
+{
+    class StaticProgram
+    {
+        static void Main(string[] args)
+        {
+
+            Student s1 = new Student("Amol", 88.99);// 4 +4 + 8+ 5 =21
+            Student s3 = new Student("John", 77.77, "USA"); // 4 +4 + 8 +3 =19
+
+            // named argument
+            Student s2 = new Student(percentage: 77.55, name: "Ajay");
+
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+            Console.WriteLine(s3);
+            Console.ReadLine();
+        }
+    }
+}
--- Student.cs
+++ Student.cs
@@ -4,38 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SkillMineProject7.OOPs
+namespace SkillMineProject7.StaticVariable
 {
     class Student
     {
-        private int roll, m1, m2, m3;
-        private string name;
-        private float total, percentage;
 
-        public Student(int roll, string name, int m1, int m2, int m3)
+        private int rollno;
+        private string name, country;
+        private double percentage;
+        private static int count;
+        public Student(string name, double percentage, string country = "India")
         {
-            this.roll = roll;
-            this.m1 = m1;
-            this.m2 = m2;
-            this.m3 = m3;
+            count++;
+            rollno = count;
             this.name = name;
+            this.percentage = percentage;
+            this.country = country;
         }
-        public void ObtainedMarks()
-        {
-            total = (m1 + m2 + m3);
-            percentage = (total) / 3;
-        }
+
         public override string ToString()
         {
-            return $"Student id  {roll}, Student name {name}, Total marks{total} & Percentage of Student {percentage}";
-        }
-        static void Main(string[] args)
-        {
-            Student s = new Student(121, "Jitesh", 50, 60, 70);
-            s.ObtainedMarks();
-            Console.WriteLine(s);
-            Console.ReadLine();
+            return $"{rollno} -> {name} -> {percentage}->{country}";
         }
     }
+
 }
-    
