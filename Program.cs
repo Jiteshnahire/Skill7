26b48507b7162656cc43f3948bc24a9c1630d3c0
Jitesh Nahire <Jiteshnahire@gmail.com>
2022-12-21 22:48:52 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,23 +1,53 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SkillMineProject7.Dynamic
+namespace ConsoleApp1._2DArray
 {
     class Program
     {
-        private dynamic data;
-        public dynamic Element { get; set; }
         static void Main(string[] args)
         {
+            ArrayList list = new ArrayList();
+            Console.WriteLine($"Capacity ->{list.Capacity}");
+            int[] arr = new int[] { 100, 200, 300 };
+            list.Add(10);  // boxing
+            list.Add("Hello");
+            list.Add(67.88);
 
-        }
+            Console.WriteLine($"Capacity ->{list.Capacity}");
+            //string name =list[1].ToString();
 
-        static dynamic Test()
-        {
-            return "Hi";
+            //double d = Convert.ToDouble(list[2]);  // unboxing
+
+            list.Insert(0, 5);
+            list.Insert(2, 15);
+
+            list.AddRange(arr);
+            Console.WriteLine($"Capacity ->{list.Capacity}");
+            // list.Remove(67.88);
+            // list.RemoveAt(3);
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("-----------------");
+            // list.RemoveRange(3, 4);
+
+            list.Clear();
+
+            Console.WriteLine($"Capacity ->{list.Capacity}");
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            list.Sort();
+            list.Reverse();
+
+
         }
     }
 }
