c148564f97116f6fa27683c706819b81ef6956f8
Jitesh Nahire <Jiteshnahire@gmail.com>
2022-12-21 22:52:38 +0530

    Add files via upload

--- Employee.cs
+++ Employee.cs
@@ -4,30 +4,77 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SkillMineProject7.Polymorphism
+namespace ConsoleApp1.Dec20
 {
-    class Employee
+    public class Employee
     {
-        /* public class Manager : Employee
-         {
-             private double food;
-             public Manager(int id, string name, double bs, double food) : base(id, name, bs)
-             {
-                 this.food = food;
-             }
-             public override void CalculateSalary()
-             {
-                 hra = basic * 0.40;
-                 da = basic * 0.20;
-                 pf = basic * 0.12;
-                 gross = (basic + hra + da + food) - pf;
-             }
+        private int esaly;
+        private string ename;
+        private int eid;
+        private int emobile;
 
-             public override string ToString()
-             {
-                 return $"Employee id {empid}, emp name {empname} & gross salary of manager {gross}";
-             }
-         }
-     }*/
+        public int Eid { get => eid; set => eid = value; }
+        public int Emobile { get => emobile; set => emobile = value; }
+        public int Esaly { get => esaly; set => esaly = value; }
+        public string Ename { get => ename; set => ename = value; }
+
+
+        public override string ToString()
+        {
+            return $"Eid {eid} Emobile{emobile} Esaly{esaly}";
+        }
+    }
+    class Student 
+    {
+        int id;
+        string name;
+
+        public Student() : this(1, "Hey!!!!!!")
+        {
+            Console.WriteLine("I am Student");
+        }
+        public Student(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+            this.Display();
+        }
+        public void Display()
+        {
+            Console.WriteLine(id + " " + name);
+        }
+        static void Main(string[] args)
+        {
+            Student s = new Student();
+            s.Display();
+            Console.WriteLine(s);
+            Console.ReadLine();
+            Employee ep = new Employee();
+
+
+            Student[] sslist = new Student[]
+            {
+                new Student{ id=121,name="Vinaya"},
+                new Student{ id=123,name="Vini"}
+
+            };
+            foreach(Student ss in sslist)
+            {
+                Console.WriteLine(sslist);
+            }
+            Employee[] emplist = new Employee[]
+            {
+            new Employee{Eid=12,Ename="Vinaya"},
+            new Employee { Eid = 25, Ename = "Xyz" },
+            new Employee{ Eid=25,Ename="Abc"},
+            };
+            
+           foreach(Employee emp in emplist)
+            {
+                Console.WriteLine(emplist);
+            }
+
+        }
     }
 }
+
---
+++ JaggedA.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Dec20
+{
+    class JaggedA
+    {
+        static void Main(string[] args)
+        {
+            //jagged array where no of cols in each row are different
+            int[][] jarray = new int[3][];
+            jarray[0] = new int[3];
+            jarray[1] = new int[2];
+            jarray[2] = new int[5];
+
+            for (int r = 0; r < jarray.GetLength(0); r++)
+            {
+                Console.WriteLine($"Enter {jarray[r].Length} elements for row {r}");
+                for (int c = 0; c < jarray[r].Length; c++)
+                {
+                    jarray[r][c] = int.Parse(Console.ReadLine());
+
+
+                }
+
+            }
+
+            for (int r = 0; r < jarray.GetLength(0); r++)
+            {
+
+                for (int c = 0; c < jarray[r].Length; c++)
+                {
+                    Console.Write(jarray[r][c] + " ");
+                }
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
